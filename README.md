1c6a106c69e96ab060f84248d08bbb07250e2883
dcmox <dancmox@comcast.net>
2020-01-28 21:59:14 -0600

    fixed binary hash tree algo

--- README.md
+++ README.md
@@ -14,5 +14,11 @@ Useful data structures and algorithms
     const tree = MoxyAlgos.BinaryHashTree(data)
 
     console.log(JSON.stringify(tree, null, 2))
-    console.log(tree.validate())
+    console.log(tree.validate()) // true
+
+    const tree = MoxyAlgos.BinaryHashTree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+
+    console.log(JSON.stringify(tree, null, 2))
+    console.log(tree.getProofs('2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3')) // Gives you the necessary node hashes to prove a transaction is valid
+    console.log(tree.verifyHash('2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3')) // Verifies hash is valid
 ```
--- lib/binaryHashTree.ts
+++ lib/binaryHashTree.ts
@@ -7,7 +7,8 @@ interface IBinaryLeaf {
 
 interface IBinaryNode {
     hash: string,
-    nodes: IBinaryNode[]
+    a: IBinaryNode,
+    b: IBinaryNode,
 }
 
 export class BinaryHashTree {
@@ -21,36 +22,83 @@ export class BinaryHashTree {
 
     public validate(tree?: any): boolean {
         if (!tree) { tree = this._tree }
-
-        if (!tree.nodes[0].a) {
-            return this._generateHash(tree.nodes[0].hash + tree.nodes[1].hash) === tree.hash
-        }
-
-        if (this._generateHash(tree.nodes[0].a.hash + tree.nodes[0].b.hash) !== tree.hash) {
-            return false
-        }
-
-        if (this._tree.nodes[0].a.nodes.length > 1) {
-            return this.validate(this._tree.nodes[0].a) && this.validate(this._tree.nodes[0].b)
-        }
+        if (!tree.a) { return this._generateHash(JSON.stringify(tree.data)) === tree.hash }
+        if (this._generateHash(tree.a.hash + tree.b.hash) !== tree.hash) { return false }
+        if (tree.a.a) { return this.validate(tree.a.a) && this.validate(tree.b.b) }
         return true
     }
 
     public tree(): any { return this._tree }
 
-    public hash(): string {
-        return this._tree.hash
+    public hash(): string { return this._tree.hash }
+
+    public verifyHash = (hash: string): any => {
+        const proofs: any[] = this.getProofs(hash)
+        const expected: string = this._tree.hash
+        let actual: string = hash
+        console.log(proofs)
+        proofs.forEach((p: any) => {
+            actual = p.position === 'left'
+            ? this._generateHash(p.proof + actual)
+            : this._generateHash(actual + p.proof)
+        })
+        return actual === expected
     }
 
-    private _createBinaryTree = (data: any[]): IBinaryNode[] => {
-        const leaves: IBinaryLeaf[] = this._generateBinaryLeaves(data)
-        let tree: any = { hash: '', nodes: this._createNodesFromLeaves(leaves) }
-        while (tree.length > 1) {
-            tree = { hash: '', nodes: this._createNodesFromLeaves(tree.nodes) }
+    public getProofs = (hash: string): any => {
+        const taProofs: any = {proof: []}
+        const tbProofs: any = {proof: []}
+        const ta = this.findProof(this._tree.a, hash, taProofs)
+        const tb = this.findProof(this._tree.b, hash, tbProofs)
+        if (ta) {
+            ta.proof.unshift({proof: this._tree.b.hash, position: 'right'})
+            return ta.proof.reverse()
+        } else if (ta) {
+            tb.proof.unshift({proof: this._tree.a.hash, position: 'left'})
+            return ta.proof.reverse()
         }
+        return false
+    }
 
-        tree.hash = this._generateHash(tree.nodes[0].a.hash + tree.nodes[0].b.hash)
-        return tree
+    public findProof = (node: any, hash: string, res: any): any => {
+        if (node.hash === hash) { return { node } }
+        if (!node.a) { return false }
+        if (node.a.hash === hash ) {
+            res.proof.push({start: true, proof: node.b.hash, position: 'right' })
+            return res
+        }
+        if (node.b.hash === hash ) {
+            res.proof.push({start: true, proof: node.a.hash, position: 'left' })
+            return res
+        }
+        const taProof = { proof: res.proof.slice() }
+        taProof.proof.push({proof: node.b.hash, position: 'right' })
+        const ta = this.findProof(node.a, hash, taProof)
+        if (ta) { return ta }
+
+        const tbProof = { proof: res.proof.slice() }
+        tbProof.proof.push({proof: node.a.hash, position: 'left' })
+        const tb = this.findProof(node.b, hash, tbProof)
+        if (tb) { return tb }
+        return false
+    }
+
+    private _createBinaryTree = (data: any[]): IBinaryNode => {
+        const leaves: IBinaryLeaf[] = this._generateBinaryLeaves(data)
+        let nodes: IBinaryNode[] = this._createNodesFromLeaves(leaves)
+        while (nodes.length > 1) {
+            const pnodes: IBinaryNode[] = []
+            for (let i = 0; i < nodes.length; i += 2) {
+                const pnode = {
+                    a: nodes[i],
+                    b: nodes[i + 1],
+                    hash: this._generateHash(nodes[i].hash + nodes[i + 1].hash),
+                }
+                pnodes.push(pnode)
+            }
+            nodes = pnodes
+        }
+        return nodes[0]
     }
 
     private _generateBinaryLeaves = (data: any[]): IBinaryLeaf[] => {
@@ -76,14 +124,18 @@ export class BinaryHashTree {
         for (let i = 0; i < nodes.length; i += 2) {
             const node = {
                 a: {
+                    a: nodes[i].a,
+                    b: nodes[i].b,
                     hash: this._generateHash(nodes[i].a.hash + nodes[i].b.hash),
-                    nodes: [nodes[i].a, nodes[i].b],
                 },
                 b: {
+                    a: nodes[i + 1].a,
+                    b: nodes[i + 1].b,
                     hash: this._generateHash(nodes[i + 1].a.hash + nodes[i + 1].b.hash),
-                    nodes: [nodes[i + 1].a, nodes[i + 1].b],
                 },
+                hash: '',
             }
+            node.hash = this._generateHash(node.a.hash + node.b.hash)
             reduced.push(node)
         }
         return reduced
